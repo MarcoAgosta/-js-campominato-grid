883c96b3c22a176fc9a502428b4605a631dfe6d3
MarcoAgosta <agostamarco@libero.it>
2022-11-09 17:24:39 +0100

    Finiti i bonus

--- JS/functions/ottantunoGrid.js
+++ JS/functions/ottantunoGrid.js
@@ -1,18 +1,18 @@
-/**La funzione crea una griglia quadrata di 100 caselle numerate, premendo le caselle cambieranno di colore e manderanno il numero assegnato in console.
+/**La funzione crea una griglia quadrata di 81 caselle numerate, premendo le caselle cambieranno di colore e manderanno il numero assegnato in console.
  * 
  */
-function myGrid() {
+function ottantunoGrid() {
 
     let grigliaQuadrata = document.createElement("div");
 
 
     grigliaQuadrata.classList.add("d-flex", "flex-wrap");
 
-    for ( let i=1 ; i <= 100; i++){
+    for ( let i=1 ; i <= 81; i++){
 
         const quadratoCento = document.createElement("div")
 
-        quadratoCento.classList.add("pt-4", "text-center", "quadrato-100", "fs-3")
+        quadratoCento.classList.add("pt-4", "text-center", "quadrato-81", "fs-3")
 
         quadratoCento.innerHTML = `${i}`
 
--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,31 @@
-const griglia = document.getElementById("griglia")
-const btnPrimary = document.getElementById("pulsante-principale")
+const griglia = document.getElementById("griglia");
+const btnPrimary = document.getElementById("pulsante-principale");
+
 
 btnPrimary.addEventListener ("click", function(){
 
-    griglia.innerHTML = ""
+    const selectValue = document.getElementById("difficoltà").value;
 
-    const grigliaCento = myGrid()
-    
-    griglia.appendChild(grigliaCento)
+    griglia.innerHTML = "";
+
+    if (parseInt(selectValue) === 100){
+
+        const grigliaCento = centoGrid();
+        
+        griglia.appendChild(grigliaCento);
+
+    } else if (parseInt(selectValue) === 81){
+
+        const grigliaOttantuno = ottantunoGrid();
+
+        griglia.appendChild(grigliaOttantuno);
+
+    } else if (parseInt(selectValue) === 49){
+
+        const grigliaQuarantanove = quarantanoveGrid();
+
+        griglia.appendChild(grigliaQuarantanove);
+            
+    }
 
 } )
